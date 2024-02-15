ef3b0f5b6f269cd17a7432b3a1cef65449e02b6c
Mateusz Battek <mateuszbattek@gmail.com>
2024-02-15 14:52:41 +0100

    moved games array to main file

--- src/components/Podium.jsx
+++ src/components/Podium.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import PodiumElement from "./PodiumElement"
 
-export default function Podium() {
+export default function Podium(props) {
   return (
     <div className="text-white py-20 mx-48 flex flex-row justify-between items-center h-3/4">
       <PodiumElement />
